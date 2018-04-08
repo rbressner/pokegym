8590816c048c273a0199fba89c94964fe3512fe1
Rachel Bressner <rachel.bressner@gmail.com>
2018-04-08 18:18:53 -0400

    design skeleton

--- rachel.js
+++ rachel.js
@@ -2,46 +2,6 @@
 // ditto https://pokeapi.co/api/v2/pokemon/132/
 // chikorita https://pokeapi.co/api/v2/pokemon/152/
 
-// Cards Entering via Pokeball Button + Pokeball/Title disappearing
-let cards = document.getElementById("cardsinfo")
-let ball = document.getElementById("pokeball")
-let head = document.getElementById("header")
-function showCards() {
-  if (cards.style.visibility === "visible") {
-    cards.style.visibility = "hidden"
-    ball.style.visibility = "visible"
-    head.style.visibility = "visible"
-  } else {
-    cards.style.visibility = "visible"
-    ball.style.visibility = "hidden"
-    head.style.visibility = "hidden"
-  }
-}
-window.addEventListener('click', (clickEvent) => {
-  cards.classList.add("show")
-})
-
-let cardback = document.getElementById("card1")
-let cardback1 = document.getElementById("card2")
-let cardback2 = document.getElementById("card3")
-function cardflip() {
-  window.addEventListener('click', (clickEvent) => {
-    cardback.classList.add("flip")
-
-  })
-}
-  function cardflip1() {
-  window.addEventListener('click', (clickEvent) => {
-    cardback1.classList.add("flip1")
-
-  })
-}
-  function cardflip2() {
-  window.addEventListener('click', (clickEvent) => {
-    cardback2.classList.add("flip2")
-
-  })
-}
 
 
 class PokemonAll {
@@ -96,7 +56,3 @@ class PokemonAll {
               callback(allPokemon)
           })
   }
-  
-
-
-
