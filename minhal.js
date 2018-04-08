ac1f7f138180504b21ebd73cf47cb6d50abacca7
Minhal <mag166@case.edu>
2018-04-08 09:40:30 -0400

    changed to axiosAll and added callback

--- minhal.js
+++ minhal.js
@@ -1,6 +1,6 @@
 
-class Pokemon{
-    constructor(name,hp,attack,defense,abilities=[]){
+class Pokemon {
+    constructor(name, hp, attack, defense, abilities = []) {
         this.hp = hp
         this.attack = attack
         this.defense = defense
@@ -8,27 +8,27 @@ class Pokemon{
         this.name = name
     }
 
-    addAbility(ability){
+    addAbility(ability) {
         this.abilities.push(ability)
     }
 }
 
-class MingKinhal{
-    constructor(){
+class MingKinhal {
+    constructor() {
         this.pokemon = []
     }
 
-    all(){
+    all() {
         return this.pokemon
     }
 
-    add(poke){
+    add(poke) {
         this.pokemon.push(poke)
     }
 
-    get(name){
-        for(let i = 0; i < this.pokemon.length; i++){
-            if(name == this.pokemon[i].name){
+    get(name) {
+        for (let i = 0; i < this.pokemon.length; i++) {
+            if (name == this.pokemon[i].name) {
                 return this.pokemon[i]
             }
         }
@@ -37,93 +37,41 @@ class MingKinhal{
 }
 
 let minhal = new MingKinhal()
-//https://pokeapi.co/api/v2/pokemon/65/
-axios.get("https://raw.githubusercontent.com/mag166/personalPokedex/master/alakazamJSON.json")
-.then((response) => {
 
+function callAxios(callback) {
+    //https://pokeapi.co/api/v2/pokemon/65/
+    let url1 = "https://raw.githubusercontent.com/mag166/personalPokedex/master/alakazamJSON.json"
 
-    let alakazam = new Pokemon(
-        data.name,
-        data.stats[5].base_stat,
-        data.stats[4].base_stat,
-        data.stats[3].base_stat,
-    )
+    //https://pokeapi.co/api/v2/pokemon/493/
+    let url2 = "https://raw.githubusercontent.com/mag166/personalPokedex/master/arceusJSON.json"
 
-    data.abilities.forEach((item) => {
-        alakazam.addAbility(item.ability.name)
-        
-        abilities.innerHTML += item.ability.name +"<br>"
-    })
+    //https://pokeapi.co/api/v2/pokemon/68/
+    let url3 = "https://raw.githubusercontent.com/mag166/personalPokedex/master/machampJSON.json"
+    axios.all([
+        axios.get(url1),
+        axios.get(url2),
+        axios.get(url3)
+    ])
+        .then(responses => {
+            responses.forEach(response => {
+                let data = response.data
+                let pokemon = new Pokemon(
+                    data.name,
+                    data.stats[5].base_stat,
+                    data.stats[4].base_stat,
+                    data.stats[3].base_stat,
+                )
+            
+                data.abilities.forEach((item) => {
+                    pokemon.addAbility(item.ability.name);
+                  })
+            
+                minhal.add(pokemon)
+            })
+            callback(minhal)
+        })
+}
 
-    minhal.add(alakazam)
-
-
-}).catch((error) => {
-    console.log(error)
+callAxios(function(people) {
+    console.log(people)
   })
-//https://pokeapi.co/api/v2/pokemon/493/
-  axios.get("https://raw.githubusercontent.com/mag166/personalPokedex/master/arceusJSON.json")
-.then((response) => {
-
-
-    let arceus = new Pokemon(
-        data.name,
-        data.stats[5].base_stat,
-        data.stats[4].base_stat,
-        data.stats[3].base_stat,
-    )
-
-    data.abilities.forEach((item) => {
-        arceus.addAbility(item.ability.name)
-        abilities.innerHTML += item.ability.name +"<br>"
-    })
-
-
-    minhal.add(arceus)
-
-
-}).catch((error) => {
-    console.log(error)
-  })
-
-//https://pokeapi.co/api/v2/pokemon/68/
-  axios.get("https://raw.githubusercontent.com/mag166/personalPokedex/master/machampJSON.json")
-  .then((response) => {
-      let data = response.data
-      let hp = document.getElementById("machamp-hp")
-      let attack = document.getElementById("machamp-attack")
-      let defense = document.getElementById("machamp-defense")
-      let abilities = document.getElementById("machamp-abilities")
-
-
-      let machamp = new Pokemon(
-          data.name,
-          data.stats[5].base_stat,
-          data.stats[4].base_stat,
-          data.stats[3].base_stat,
-      )
-
-      data.abilities.forEach((item) => {
-          machamp.addAbility(item.ability.name)
-          abilities.innerHTML += item.ability.name +"<br>"
-      })
-
-
-      minhal.add(machamp)
-      hp.innerHTML = machamp.hp
-      attack.innerHTML = machamp.attack
-      defense.innerHTML = machamp.defense
-
-      console.log(minhal.get("machamp"))
-
-
-  }).catch((error) => {
-      console.log(error)
-    })
-
-    console.log(minhal.get("machamp"))
-
-
-
-
-   
